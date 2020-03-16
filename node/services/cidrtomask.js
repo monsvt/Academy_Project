f8951751e3bff37272687e6da97186ae08eab7dc
monsvt <sofia_mvt@hotmail.com>
2020-03-16 21:01:14 +0000

    initial code

--- node/services/cidrtomask.js
+++ node/services/cidrtomask.js
@@ -1,5 +1,15 @@
 export const cidrToMaskFunction = (value) => {
   //console.log(value);
-  return value;
-
+  var mask= []
+  var i;
+  
+  for (i=0;i<4;i++){
+    if (value>=8 && value<=32) {
+    var n=Math.min(value,8);
+    mask.push(256-Math.pow(2,8-n));
+    value-=n;
+    return mask.join('.');
+    }
+  }
+    return "Invalid"
 }
